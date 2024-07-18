001742da226b225c00f532692e7abf9298dc2cd0
bishop254 <karanjachiuri@gmail.com>
2024-07-18 14:09:17 +0300

    Updates

--- EcommerceProject/codebase/backend/config/dbConnect.js
+++ EcommerceProject/codebase/backend/config/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDatabase = () => {
-  let DB_URI = "mongodb://host.docker.internal:27017/shopit-v2";
+  let DB_URI = "mongodb://102.23.120.135:27017/shopit-v2";
 
   if (process.env.NODE_ENV === "DEVELOPMENT") DB_URI = process.env.DB_LOCAL_URI;
   if (process.env.NODE_ENV === "PRODUCTION") DB_URI = process.env.DB_URI;
