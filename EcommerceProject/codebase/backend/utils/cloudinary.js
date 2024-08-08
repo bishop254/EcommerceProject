0862cf6e72cde17fe8b60cffff1a6377f568d222
bishop254 <karanjachiuri@gmail.com>
2024-08-08 14:57:10 +0300

    Update on image upload and volume persistence

--- EcommerceProject/codebase/backend/utils/cloudinary.js
+++ EcommerceProject/codebase/backend/utils/cloudinary.js
@@ -1,27 +1,28 @@
-import cloudinary from "cloudinary";
+import {v2 as cloudinary} from "cloudinary";
 import dotenv from "dotenv";
 
-dotenv.config({ path: "backend/config/config.env" });
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+dotenv.config({ path: "./../config/config.env" });
 
 export const upload_file = (file, folder) => {
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true,
+  });
   return new Promise((resolve, reject) => {
     cloudinary.uploader.upload(
       file,
-      (result) => {
+
+      {
+        resource_type: "auto",
+        folder,
+      },
+      (err, result) => {
         resolve({
           public_id: result.public_id,
           url: result.url,
         });
-      },
-      {
-        resource_type: "auto",
-        folder,
       }
     );
   });
